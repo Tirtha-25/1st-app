498d955964731b5331d4742113dcc0cce4efde61
Tirtha-25 <82389327+Tirtha-25@users.noreply.github.com>
2026-02-21 21:41:38 +0000

    Deploying to gh-pages from @ Tirtha-25/1st-app@48d98b556fc5f72532c9f86bca80b876ef396fa7 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3763243229" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1183760556" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
